37c3515ddfdd47d2a7a5a4d220900eb78ddc2ca8
Brando Meniconi <meniconi.brando@gmail.com>
2014-05-13 12:47:02 +0200

    used yii imports and added css selection options

--- YiiMailerInline.php
+++ YiiMailerInline.php
@@ -1,40 +1,49 @@
 <?php
 
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'CssToInlineStyles'.DIRECTORY_SEPARATOR.'CssToInlineStyles.php';
+Yii::setPathOfAlias('TijsVerkoyen', Yii::getPathOfAlias('ext'));
+
+Yii::import('ext.YiiMailerInline.YiiMailer');
+Yii::import('TijsVerkoyen.CssToInlineStyles.CssToInlineStyles');
 
 class YiiMailerInline extends YiiMailer implements IApplicationComponent  {
 
     public $defaults;
     public $managementAddress;
-    
+    public $cssFileName = 'mail.css';
+    public $cssFilePath;
+
     private $_initialized=false;
-    
+
     public function init()
     {
-            
+
         if(isset($this->defaults['bcc'])){
             $this->setBcc($this->defaults['bcc']);
         }
-        
-        if(isset($this->defaults['from']) && isset($this->defaults['fromName'])){
-            $this->setFrom($this->defaults['from'], $this->defaults['fromName']);            
+
+        if(!$this->cssFilePath){
+            $this->cssFilePath = Yii::app()->theme->getBasePath().DIRECTORY_SEPARATOR.'css'.DIRECTORY_SEPARATOR;
         }
-            
+
+        if(isset($this->defaults['from']) && isset($this->defaults['fromName'])){
+            $this->setFrom($this->defaults['from'], $this->defaults['fromName']);
+        }
+
         $this->_initialized=true;
     }
-    
+
     public function getIsInitialized()
     {
-            return $this->_initialized;
-    }    
+        return $this->_initialized;
+    }
 
     public function render() {
         parent::render();
-        
-        $inliner = new \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles($this->Body, file_get_contents(Yii::app()->theme->getBasePath()."/css/mail.css"));
-        
+
+        $inliner = new \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles($this->Body, file_get_contents($this->cssFilePath.$this->cssFileName));
+
         $this->Body = $inliner->convert();
-        
+
     }
-    
+
 }
